f17ba3f0b4b285c23a9cc66f097b7e35bbd03497
KEGS Young Engineers <youngengineers@kegs.org.uk>
2017-02-02 08:20:44 +0000

    Added to timer file

--- timer.py
+++ timer.py
@@ -1,3 +1,34 @@
+#This file has merge conflicts - please check through it
+#Updated upstream
+import os, sys
+import pygame
+from pygame.locals import *
+import time
+
+class Timer(pygame.sprite.Sprite):
+
+    def __init__ (self, screen, width, height):
+        pygame.init()
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode((800, 480))
+
+    def Main(self):
+        answered = False
+
+        while 1:
+            self.screen.fill((255,249,216))
+
+            timerImage = pygame.sprite.Sprite()
+            timerImage.image = pygame.image.load("/home/pi/2016-17/blue-car-top-view-hi.png")
+            screen.blit(timerImage, (width/2, height/2))
+
+            pygame.display.update
+
+Window = Timer()
+Window.Main()
+
+#Stashed changes
 import time
 
 t = 0
@@ -9,5 +40,3 @@ while pause == False:
     print (t)
     if t == 10:
         pause = True
-
-
